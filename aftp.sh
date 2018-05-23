68f257058deceb0c9266e9d0ec3f1db5391b51e5
Zach Cummings <zwcummings@gmail.com>
2018-05-23 13:01:25 -0400

    began work on intergrating inspec testing

--- aftp.sh
+++ aftp.sh
@@ -17,31 +17,32 @@
 # Failure to do so will lead to surprisingly high bills --> load balancers != cheap
 
 echo "## Checking for dependencies ##"
-# checks for AWS Command Line Tools, Terraform, and TFLint
+# checks for AWS Command Line Tools, Terraform, TFLint, and Inspec
 command -v aws >/dev/null 2>&1 || { echo "AWS Command Line Tools are required but not installed.  Aborting. Please install the AWS Command Line Tools." >&2; exit 1; }
 command -v terraform >/dev/null 2>&1 || { echo "Terraform is required but it's not installed.  Aborting. Please install Terraform." >&2; exit 1; }
 command -v tflint >/dev/null 2>&1 || { echo "TFLint is required but it's not installed.  Aborting. Please install TFLint." >&2; exit 1; }
+command -v inspec >/dev/null 2>&1 || { echo "Inspec is required but it's not installed.  Aborting. Please install TFLint." >&2; exit 1; }
 
 echo "## Building something AWSome ##"
 # look for an existing key and if not there, create one with the appropriate permissions
-(ls ~/.ssh/zc_miniproject_key.pem >> /dev/null 2>&1 && echo "## Skipping key generation ##") || (echo "## Creating new key ##"; aws ec2 --region us-east-1 create-key-pair --key-name zc_miniproject_key --query 'KeyMaterial' --output text > ~/.ssh/zc_miniproject_key.pem; chmod 400 ~/.ssh/zc_miniproject_key.pem; echo "Key created in ~/.ssh with permissions set to 400")
+(ls ~/.ssh/project_key.pem >> /dev/null 2>&1 && echo "## Skipping key generation ##") || (echo "## Creating new key ##"; aws ec2 --region us-east-1 create-key-pair --key-name project_key --query 'KeyMaterial' --output text > ~/.ssh/project_key.pem; chmod 400 ~/.ssh/project_key.pem; echo "Key created in ~/.ssh with permissions set to 400")
 
 echo "## Checking if ssh-agent is running ##"
 # check is ssh-agent is running and if no, start ssh-agent then add the new key
-(pgrep ssh-agent > /dev/null 2>&1 && echo "## ssh-agent is running ##") || (echo "## Starting ssh-agent ##"; eval `ssh-agent -s`; echo "##  Adding new key ##"; ssh-add ~/.ssh/zc_miniproject_key.pem)
+(pgrep ssh-agent > /dev/null 2>&1 && echo "## ssh-agent is running ##") || (echo "## Starting ssh-agent ##"; eval `ssh-agent -s`; echo "##  Adding new key ##"; ssh-add ~/.ssh/project_key.pem)
 
 echo "## Linting the Terraform templates ##"
 # run TFLint against our templates to make sure our code follows best practices and to look for errors that cannot be detected by 'terraform plan' 
 tflint
-
-echo "## Testing the infrastucture with Inspec"
-
 
 # assuming tflint produces no errors, execute 'terraform apply'
 echo "##  Assembling the universe ##"
 terraform init
 terraform apply
 
-
+# testing with Inspec
+echo "## Testing the infrastucture with Inspec"
+sleep 5m # gives the ec2 instances time to finish initialization
+inspec exec test.rb
 
 
